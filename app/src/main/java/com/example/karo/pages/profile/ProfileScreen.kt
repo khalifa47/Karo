599ecbf5c8e621e5c6a768148562881a6a943b35
Nabcellent <nabcellent.dev@gmail.com>
2022-11-25 05:19:04 +0300

    Implements profile page
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- app/src/main/java/com/example/karo/pages/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/karo/pages/profile/ProfileScreen.kt
@@ -5,28 +5,28 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.karo.R
 import com.example.karo.components.CustomOutlinedTextField
-import com.example.karo.ui.theme.KaroTheme
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import com.example.karo.utils.Helpers
 
 @Composable
 fun ProfileScreen() {
+    Helpers.showToast(LocalContext.current, "Invalid Credentials!")
     Scaffold { padding ->
         Profile(
             userContent = { user ->
